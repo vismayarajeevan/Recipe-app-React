ff91abbed32e64b3498df72e2a5eba5befcbb98e
vismayarajeevan <vismayarajeevan15@gmail.com>
2025-01-13 22:10:51 +0530

    completed v1

--- src/services/serverURL.js
+++ src/services/serverURL.js
@@ -1,6 +1,6 @@
 // 2. create a file to hold server url for easy at deploy time
 // file to save and use serverurl 
 
-const SERVERURL = "http://localhost:3000"
+const SERVERURL = "https://recipeapp-server-5p1q.onrender.com"
 
 export default SERVERURL
